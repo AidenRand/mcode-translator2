d82bed0a663f47bb3c0871bdd3bff16c3f7b5ae7
aiden <aidenrphotos@gmail.com>
2023-03-02 11:07:55 -0500

    Add alphabet and clean code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # mcode-translator2
+A python version of my C++ morse code translator for comparison | 10:55am | 3-1-23
--- main.py
+++ main.py
@@ -1,7 +1,84 @@
 def get_input():
-    userInput = input("\nEnter morse code: ")
-
-    match userInput:
-        case ".-":
-            print('A')
+    print('----------------------------')
+    print('   MORSE CODE TRANSLATOR')
+    print('----------------------------')
+    print('     Enter (q) to quit')
+    while True:
+        userInput = input("\nEnter morse code: ")
+        match userInput:
+            case '.-':
+                print('A')
+            case '-...':
+                print('B')
+            case '-.-.':
+                print('C')
+            case '-..':
+                print('D')
+            case '.':
+                print('E')
+            case '..-.':
+                print('F')
+            case '--.':
+                print('G')
+            case '....':
+                print('H')
+            case '..':
+                print('I')
+            case '.---':
+                print('J')
+            case '-.-':
+                print('K')
+            case '.-...':
+                print('L')
+            case '--':
+                print('M')
+            case '-.':
+                print('N')
+            case '---':
+                print('O')
+            case '.--.':
+                print('P')
+            case '--.-':
+                print('Q')
+            case '.-.':
+                print('R')
+            case '...':
+                print('S')
+            case '-':
+                print('T')
+            case '..-':
+                print('U')
+            case '...-':
+                print('V')
+            case '.--':
+                print('W')
+            case '-..-':
+                print('X')
+            case '-.--':
+                print('Y')
+            case '--..':
+                print('Z')
+            case '.----':
+                print('1')
+            case '..---':
+                print('2')
+            case '...--':
+                print('3')
+            case '....-':
+                print('4')
+            case '.....':
+                print('5')
+            case '-....':
+                print('6')
+            case '--...':
+                print('7')
+            case '---..':
+                print('8')
+            case '----.':
+                print('9')
+            case '-----':
+                print('0')
+            case 'q':
+                break
+        
 get_input()
